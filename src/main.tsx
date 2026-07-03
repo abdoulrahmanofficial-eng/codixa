ab66ab33617f16dd2c539c02471e53f33cd4c11f
Dev Abdulrahman <abdoulrahman.official@gmail.com>
2026-07-04 01:42:16 +0300

    fix: unregister old PWA service workers to clear stale cache

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Unregister any old PWA service workers that might cache stale app
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((reg) => reg.unregister());
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
