b53e0d5bb5d6a54708dae82777ffd9769cbee48a
Dev Abdulrahman <abdoulrahman.official@gmail.com>
2026-07-04 03:15:12 +0300

    fix: set browserLocalPersistence to keep user logged in between sessions

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -14,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
 export const rtdb = getDatabase(app);
 export default app;
